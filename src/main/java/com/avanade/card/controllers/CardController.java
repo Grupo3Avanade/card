27ef2fa7f0522f7b1f060e76e4fb5d14a71cb2da
Angela Hempkmeier <aggsdpz@gmail.com>
2023-09-07 17:07:02 -0300

    feat(entities, repositories, service) continuing the implementation

---
+++ src/main/java/com/avanade/card/controllers/CardController.java
@@ -0,0 +1,45 @@
+package com.avanade.card.controllers;
+
+import com.avanade.card.payloads.request.CardRequest;
+import com.avanade.card.payloads.response.CardResponse;
+import com.avanade.card.services.CardService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("cards")
+public class CardController {
+    private final CardService service;
+
+    public CardController(CardService service) {
+        this.service = service;
+    }
+
+
+    @PostMapping
+    public CardResponse create(@RequestBody CardRequest dto){
+        return service.create(dto);
+    }
+
+    @GetMapping("/user/{id}")
+    public List<CardResponse> getAllByUser(@PathVariable UUID id){
+        return service.getAllByUser(id);
+    }
+
+    @GetMapping("/{id}")
+    public CardResponse getOne(@PathVariable UUID id){
+        return service.getOne(id);
+    }
+
+    @PutMapping("/{id}")
+    public CardResponse edit(@PathVariable UUID id, @RequestBody CardRequest cartaoRequest){
+        return service.update(id, cartaoRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable UUID id){
+        return service.delete(id);
+    }
+}
